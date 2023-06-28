fcab17c941a77d534b30386c721c9167ab4bf895
Justin Firth <jmfirth@gmail.com>
2023-06-27 23:03:44 -0400

    Update to web extension

--- README.md
+++ README.md
@@ -6,16 +6,20 @@ Space Block Jumper helps you navigate through space-delimited blocks.
 
 ## Features
 
-* `spaceBlockJumper.moveUp`: Move up a space block (often `ctrl+up`)
-* `spaceBlockJumper.moveDown`: Move down a space block (often `ctrl+down`)
-* `spaceBlockJumper.selectUp`: Select up a space block (often `shift+ctrl+up`)
-* `spaceBlockJumper.selectDown`: Select down a space block (often `shift+ctrl+down`)
+- `spaceBlockJumper.moveUp`: Move up a space block (often `ctrl+up`)
+- `spaceBlockJumper.moveDown`: Move down a space block (often `ctrl+down`)
+- `spaceBlockJumper.selectUp`: Select up a space block (often `shift+ctrl+up`)
+- `spaceBlockJumper.selectDown`: Select down a space block (often `shift+ctrl+down`)
 
 ## Known Issues
 
 None at this time.
 
 ## Release Notes
+
+### 2.0.0
+
+Update to web extension
 
 ### 1.2.0
 
--- src/extension.ts
+++ src/extension.ts
@@ -1,61 +1,99 @@
 "use strict";
 
-import * as readline from "readline";
 import * as vscode from "vscode";
 
-function nextPosition(document: vscode.TextDocument, position: vscode.Position, up: boolean = false): number {
-    const step = up ? -1 : 1;
-    const boundary = up ? 0 : document.lineCount - 1;
-    let index = position.line + step;
-    if (position.line === boundary) return position.line;
-    return afterBlock(document, step, boundary, position.line);
+function nextPosition(
+  document: vscode.TextDocument,
+  position: vscode.Position,
+  up: boolean = false
+): number {
+  const step = up ? -1 : 1;
+  const boundary = up ? 0 : document.lineCount - 1;
+  if (position.line === boundary) return position.line;
+  return afterBlock(document, step, boundary, position.line);
 }
 
-function afterBlock(document: vscode.TextDocument, step: number, boundary: number, index: number, startedBlock: boolean = false): number {
-    const line = document.lineAt(index);
-    return index === boundary || startedBlock && line.isEmptyOrWhitespace
-        ? index
-        : afterBlock(document, step, boundary, index + step, startedBlock || !line.isEmptyOrWhitespace);
+function afterBlock(
+  document: vscode.TextDocument,
+  step: number,
+  boundary: number,
+  index: number,
+  startedBlock: boolean = false
+): number {
+  const line = document.lineAt(index);
+  return index === boundary || (startedBlock && line.isEmptyOrWhitespace)
+    ? index
+    : afterBlock(
+        document,
+        step,
+        boundary,
+        index + step,
+        startedBlock || !line.isEmptyOrWhitespace
+      );
 }
 
 function anchorPosition(selection: vscode.Selection) {
-    return selection.active.line === selection.end.line ? selection.start : selection.end
+  return selection.active.line === selection.end.line
+    ? selection.start
+    : selection.end;
 }
 
-function markSelection(editor: vscode.TextEditor, next: number, anchor?: vscode.Position) {
-        const active = editor.selection.active.with(next, 0);
-        editor.selection = new vscode.Selection(anchor || active, active);
-        editor.revealRange(new vscode.Range(active, active));
+function markSelection(
+  editor: vscode.TextEditor,
+  next: number,
+  anchor?: vscode.Position
+) {
+  const active = editor.selection.active.with(next, 0);
+  editor.selection = new vscode.Selection(anchor || active, active);
+  editor.revealRange(new vscode.Range(active, active));
 }
 
 export function activate(context: vscode.ExtensionContext) {
-    context.subscriptions.push(vscode.commands.registerCommand("spaceBlockJumper.moveUp", () => {
-        const editor = vscode.window.activeTextEditor;
-        markSelection(editor, nextPosition(editor.document, editor.selection.active, true));
-    }));
+  context.subscriptions.push(
+    vscode.commands.registerCommand("spaceBlockJumper.moveUp", () => {
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) return;
+      markSelection(
+        editor,
+        nextPosition(editor.document, editor.selection.active, true)
+      );
+    })
+  );
 
-    context.subscriptions.push(vscode.commands.registerCommand("spaceBlockJumper.moveDown", () => {
-        const editor = vscode.window.activeTextEditor;
-        markSelection(editor, nextPosition(editor.document, editor.selection.active, false));
-    }));
+  context.subscriptions.push(
+    vscode.commands.registerCommand("spaceBlockJumper.moveDown", () => {
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) return;
+      markSelection(
+        editor,
+        nextPosition(editor.document, editor.selection.active, false)
+      );
+    })
+  );
 
-    context.subscriptions.push(vscode.commands.registerCommand("spaceBlockJumper.selectUp", () => {
-        const editor = vscode.window.activeTextEditor;
-        markSelection(
-            editor,
-            nextPosition(editor.document, editor.selection.active, true),
-            anchorPosition(editor.selection)
-        );
-    }));
+  context.subscriptions.push(
+    vscode.commands.registerCommand("spaceBlockJumper.selectUp", () => {
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) return;
+      markSelection(
+        editor,
+        nextPosition(editor.document, editor.selection.active, true),
+        anchorPosition(editor.selection)
+      );
+    })
+  );
 
-    context.subscriptions.push(vscode.commands.registerCommand("spaceBlockJumper.selectDown", () => {
-        const editor = vscode.window.activeTextEditor;
-        markSelection(
-            editor,
-            nextPosition(editor.document, editor.selection.active, false),
-            anchorPosition(editor.selection)
-        );
-    }));
+  context.subscriptions.push(
+    vscode.commands.registerCommand("spaceBlockJumper.selectDown", () => {
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) return;
+      markSelection(
+        editor,
+        nextPosition(editor.document, editor.selection.active, false),
+        anchorPosition(editor.selection)
+      );
+    })
+  );
 }
 
-export function deactivate() { }
+export function deactivate() {}
